496f77811f68abbc6a253c95fcd4f9192f80b9f7
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2019-06-29 12:36:13 -0300

    Adicionando paginação e CORS

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const porta = 3001
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const requireDir = require('require-dir')
 const routes = require('./src/routes')
 
 // Iniciando o App
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 //Iniciando o DB
-mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
 requireDir('./src/models')
 
 // Rotas

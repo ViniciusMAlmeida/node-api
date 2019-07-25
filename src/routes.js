9bbef8ce7367153f986b39ecf6e38d94f237fd9b
vinicius.almeida <vinicius.almeida@coopertec.com.br>
2019-07-25 17:32:40 -0300

    autenticação com JWT, proteção de rotas sem token

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const routes = express.Router()
+const authMiddleware = require('./middlewares/auth')
+const UserController = require('./controllers/UserController')
 const ProductController = require('./controllers/ProductController')
 
-routes.get('/products', ProductController.index)
-routes.get('/products/:id', ProductController.show)
-routes.post('/products', ProductController.store)
-routes.put('/products/:id', ProductController.update)
-routes.delete('/products/:id', ProductController.destroy)
+routes.post('/user/register', UserController.register)
+routes.post('/user/authenticate', UserController.authenticate)
+routes.get('/user/me', authMiddleware, UserController.me)
+
+routes.get('/products', authMiddleware, ProductController.index)
+routes.get('/products/:id', authMiddleware, ProductController.show)
+routes.post('/products', authMiddleware, ProductController.store)
+routes.put('/products/:id', authMiddleware, ProductController.update)
+routes.delete('/products/:id', authMiddleware, ProductController.destroy)
 
 module.exports = routes
